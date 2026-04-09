40434cbccb502213bb25e1767fdb2a4267914a9e
Sergi Albalat <sergi.albalat.7e8@itb.cat>
2026-04-09 17:35:57 +0200

    MaxLVLInfo&MoreLVLAdded

--- Assets/Scripts/UI/UpgradeMenu.cs
+++ Assets/Scripts/UI/UpgradeMenu.cs
@@ -11,8 +11,8 @@ public class UpgradeMenu : MonoBehaviour
     [SerializeField] private SOEquipment bootsData;
     private void Update()
     {
-        weaponText.text = GameManager.gm.WeaponNextLvlCost + " - Upgrade Weapon: LVL " + GameManager.gm.WeaponLevel;
-        bootsText.text = GameManager.gm.BootsNextLvlCost + " - Upgrade Boots: LVL " + GameManager.gm.BootsLevel;
+        weaponText.text = GameManager.gm.WeaponLevel != weaponsData.levels.Count ? GameManager.gm.WeaponNextLvlCost + " - Upgrade Weapon: LVL " + GameManager.gm.WeaponLevel : "Max LVL Weapon Reached";
+        bootsText.text = GameManager.gm.BootsLevel != bootsData.levels.Count ? GameManager.gm.BootsNextLvlCost + " - Upgrade Boots: LVL " + GameManager.gm.BootsLevel : "Max LVL Boots Reached";
         weaponInfo.text = "Current Damage: " + weaponsData.levels[GameManager.gm.WeaponLevel - 1];
         bootsInfo.text = "Current Speed Boost: +" + bootsData.levels[GameManager.gm.BootsLevel - 1] + "%";
     }
